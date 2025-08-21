c287289ded30bdb88fdd09d881da299c214d1569
mussa khan <raja.mussa.khan786@gmail.com>
2025-08-21 23:39:50 +0500

    run_all.ps1

--- run_all.ps1
+++ run_all.ps1
@@ -1,6 +1,6 @@
 # Step 1: Upgrade pip and install dependencies
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirments.txt
 
 # Step 2: Run the full pipeline
 python run_all.py
